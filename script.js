35db3c868f2917deeba1ac859caa4d937fff7d73
Karolis Davli <kdavlijevas@gmail.com>
2023-05-11 14:18:56 +0200

    random color generator

--- script.js
+++ script.js
@@ -9,20 +9,39 @@ const gridBtn = document.createElement("button");
 gridBtn.textContent = "Generate new grid";
 gridBtn.addEventListener("click", makeGrid);
 
+const rainbowBtn = document.createElement("button");
+rainbowBtn.textContent = "Make it colorful";
+rainbowBtn.addEventListener("click", rainbow);
+
+// Make it colorful
+function rainbow() {
+  const rainbowSquares = document.querySelectorAll(".square");
+  rainbowSquares.forEach((square) => {
+    square.addEventListener("mouseover", color);
+  });
+}
+
+function color(e) {
+  // Generate random color
+  const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  e.target.style.backgroundColor = randomColor;
+  console.log(randomColor);
+}
+
 // Make Grid function
 function makeGrid() {
   let size = prompt("How many squares on x and y axis");
   if (size > 32) {
-    prompt("Maximum is 32");
-    return;
-  }
-  clearGrid();
-  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-  for (let i = 0; i < size * size; i++) {
-    const square = document.createElement("div");
-    square.className = "square";
-    square.addEventListener("mouseover", change);
-    grid.append(square);
+    alert("Maximum is 32");
+  } else {
+    clearGrid();
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    for (let i = 0; i < size * size; i++) {
+      const square = document.createElement("div");
+      square.className = "square";
+      square.addEventListener("mouseover", change);
+      grid.append(square);
+    }
   }
 }
 
@@ -41,8 +60,7 @@ for (let i = 0; i < 256; i++) {
 
 // Change color on hover
 function change(e) {
-  console.log("hey ho");
   e.target.style.backgroundColor = "green";
 }
 
-app.append(grid, gridBtn);
+app.append(grid, gridBtn, rainbowBtn);
